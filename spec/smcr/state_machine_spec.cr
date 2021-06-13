0a852efa4273daffeaf48ef6f78ecfc5bce7c89d
Daniel Huffman <drhuffman12@gmail.com>
2021-06-13 02:27:19 -0400

    drhuffman12_switch_from_symbols_to_enums refactor to user Enum's; TODO: history

--- spec/smcr/state_machine_spec.cr
+++ spec/smcr/state_machine_spec.cr
@@ -3,100 +3,43 @@ require "./../spec_helper"
 # TODO: extract to a separate shard
 
 Spectator.describe Smcr::StateMachine do
-  context "given no params" do
-    let(state_machine) { Smcr::StateMachine.new }
+  context "example off, red, green, blue" do
+    enum LightColors
+      Off
+      Red
+      Green
+      Blue
+    end
 
-    let(states_allowed_expected) { Smcr::StatesAllowed.new }
-    let(state_default_expected) { Smcr::StateMachine::STATE_NOT_SET }
-    let(history_size_expected) { 10.to_u8 }
-    let(tick_expected) { 0 }
-    let(state_expected) { :state_not_set }
-    let(history_expected) { Smcr::StateChangeHistory.new }
-    let(paths_allowed_expected) { Smcr::PathsAllowed.new }
-
-    let(errors_expected) {
-      {
-        :states_allowed => "must be populated with states",
-        :state_default  => "must be one of state_allowed",
-        :states         => "must be one of state_allowed",
-        :paths_allowed  => "must be an mapping of state to array of states",
-      }
+    let(state_machine) {
+      Smcr::StateMachine(LightColors).new
     }
+    # let(state_class_expected) { LightColors }
 
     describe "to/from/to json" do
       let(to_json) { state_machine.to_json }
-      let(from_json) { Smcr::StateMachine.from_json(to_json) }
+      let(from_json) { Smcr::StateMachine(LightColors).from_json(to_json) }
       let(to_json2) { from_json.to_json }
 
-      # # simple check
-      # pending "JSON values match" do
-      #   expect(to_json).to eq(to_json2)
-      # end
-
-      # it "instance sizes match" do
-      #   expect(state_machine).not_to be_nil
-      #   # sm = Smcr::StateMachine.new
-      #   state_machine_size = instance_sizeof(Smcr::StateMachine.new)
-      #   from_json_size = instance_sizeof(from_json)
-      #   expect(state_machine_size).to eq(from_json_size)
-      # end
-    end
-
-    describe "#initialize" do
-      context "sets values for" do
-        it "states_allowed" do
-          expect(state_machine.states_allowed).to eq(states_allowed_expected)
-        end
-
-        it "state_default" do
-          expect(state_machine.state_default).to eq(state_default_expected)
-        end
-
-        it "history_size" do
-          expect(state_machine.history_size).to eq(history_size_expected)
-        end
-
-        it "tick" do
-          expect(state_machine.tick).to eq(tick_expected)
-        end
-
-        it "state" do
-          expect(state_machine.state).to eq(state_expected)
-        end
-
-        it "history" do
-          expect(state_machine.history).to eq(history_expected)
-        end
-
-        it "paths_allowed" do
-          expect(state_machine.paths_allowed).to eq(paths_allowed_expected)
-        end
-
-        it "errors" do
-          expect(state_machine.errors).to eq(errors_expected)
-        end
+      # For now, just a simple check to make sure to/from JSON doesn't error!
+      # We probably should check various keys and values individually.
+      it "JSON values match" do
+        expect(to_json).to eq(to_json2)
       end
     end
-  end
 
-  context "example off, red, green, blue" do
     describe "#initialize" do
-      let(states_allowed) { [:off, :red, :green, :blue] }
-      let(state_default) { :off }
+      # let(states_allowed) { [:off, :red, :green, :blue] }
+      let(state_names_expected) { ["Off", "Red", "Green", "Blue"] }
+      let(state_values_expected) { [LightColors::Off, LightColors::Red, LightColors::Green, LightColors::Blue] }
+      let(state_internal_values_expected) { [0, 1, 2, 3] }
+      let(state_default) { LightColors::Off }
       let(history_size) { 2.to_u8 }
       let(tick) { 3 }
-      let(state) { :blue }
-      let(history) { nil }
-      let(paths_allowed) {
-        {
-          :off   => [:red, :green, :blue],
-          :red   => [:off, :green, :blue],
-          :green => [:off, :red, :blue],
-          :blue  => [:off, :red, :green],
-        }
-      }
+      let(state) { LightColors::Blue }
+      # let(history) { nil } # TODO!
+      let(paths_allowed) { nil }
 
-      let(states_allowed_expected) { states_allowed }
       let(state_default_expected) { state_default }
       let(history_size_expected) { history_size }
       let(tick_expected) { tick }
@@ -104,26 +47,40 @@ Spectator.describe Smcr::StateMachine do
       let(history_expected) { Smcr::StateChangeHistory.new }
 
       let(paths_allowed_expected) {
-        paths_allowed
+        {
+          LightColors::Off.value => [LightColors::Red.value, LightColors::Green.value, LightColors::Blue.value],
+        }
       }
 
       let(errors_expected) { Smcr::CurrentErrors.new }
 
       let(state_machine) {
         Smcr::StateMachine.new(
-          states_allowed: states_allowed,
+          # states_allowed: states_allowed,
           state_default: state_default,
           history_size: history_size,
           tick: tick,
           state: state,
-          history: history,
+          # history: history, # TODO!
           paths_allowed: paths_allowed
         )
       }
 
       context "sets values for" do
-        it "states_allowed" do
-          expect(state_machine.states_allowed).to eq(states_allowed_expected)
+        # it ".state_class" do
+        #   expect(state_machine.class.state_class).to eq(state_class_expected)
+        # end
+
+        it ".state_names" do
+          expect(state_machine.class.state_names).to eq(state_names_expected)
+        end
+
+        it ".state_values" do
+          expect(state_machine.class.state_values).to eq(state_values_expected)
+        end
+
+        it ".state_internal_values" do
+          expect(state_machine.class.state_internal_values).to eq(state_internal_values_expected)
         end
 
         it "state_default" do
@@ -142,9 +99,9 @@ Spectator.describe Smcr::StateMachine do
           expect(state_machine.state).to eq(state_expected)
         end
 
-        it "history" do
-          expect(state_machine.history).to eq(history_expected)
-        end
+        # it "history" do # TODO!
+        #   expect(state_machine.history).to eq(history_expected)
+        # end
 
         it "paths_allowed" do
           expect(state_machine.paths_allowed).to eq(paths_allowed_expected)
--- src/smcr/state_machine.cr
+++ src/smcr/state_machine.cr
@@ -1,83 +1,90 @@
 module Smcr
-  alias CurrentErrors = Hash(Symbol, String)
-  alias State = Symbol
+  alias StateValue = Int32 # aka internal representation of an Enum's value
+
+  # alias CurrentErrors = Hash(Symbol, String)
+  # alias State = T
   alias Tick = Int32        # Bigger?
   alias HistorySize = UInt8 # Bigger?
   # alias History = Array(StateChanges)
 
-  alias StatesAllowed = Array(State)
-  alias PathsAllowed = Hash(State, StatesAllowed)
+  alias StatesAllowed = Array(StateValue)
+  alias PathsAllowed = Hash(StateValue, StatesAllowed)
 
   alias StateChangedAttempt = NamedTuple(
     forced: Bool,
-    from: {state: State, tick: Tick},
-    to: {state: State, tick: Tick})
+    from: {state_value: StateValue, tick: Tick},
+    to: {state_value: StateValue, tick: Tick})
   alias CallbackResponse = NamedTuple(
-    suceeded: Bool,
+    succeeded: Bool,
     code: Int32, # e.g.: mimic HTTP codes
     message: String)
   alias StateChange = NamedTuple(
-    state_became: State,
+    state_value_became: StateValue,
     tick_became: Tick,
     state_change_attempted: StateChangedAttempt,
     callback_response: CallbackResponse)
   alias StateChangeHistory = Array(StateChange)
 
-  class StateMachine
+  alias CurrentErrors = Hash(String, String)
+
+  class StateMachine(State)
     include JSON::Serializable
 
-    STATE_NOT_SET = :state_not_set
+    # STATE_NOT_SET = :state_not_set
+    ERROR_KEY_PATHS_ALLOWED = "paths_allowed"
 
-    getter states_allowed : StatesAllowed
+    # getter states_allowed : Array(T) = T.values
     getter state_default : State
     getter history_size : HistorySize
     getter tick : Tick
     getter state : State
-    getter history : StateChangeHistory
+    # getter history : StateChangeHistory # TODO!
     getter paths_allowed : PathsAllowed
 
     getter errors : CurrentErrors
 
-    # ameba:disable Metrics/CyclomaticComplexity
+    def self.state_class
+      State.class # Needed? Maybe just in case we want to programmatically remind our selves what the 'State' class is?
+    end
+
+    def self.state_names
+      State.names # Needed?
+    end
+
+    def self.state_values
+      State.values # Needed?
+    end
+
+    def self.state_internal_values
+      State.values.map(&.value) # Needed?
+    end
+
     def initialize(
-      states_allowed : StatesAllowed? = nil,
+      # states_allowed : StatesAllowed? = nil,
       state_default : State? = nil,
       history_size : HistorySize? = nil,
       tick : Tick? = nil,
       state : State? = nil,
-      history : StateChangeHistory? = nil,
+      # history : StateChangeHistory? = nil, # TODO!
       paths_allowed : PathsAllowed? = nil
     )
-      @states_allowed = states_allowed ? states_allowed : StatesAllowed.new
+      # @states_allowed = states_allowed ? states_allowed : StatesAllowed.new
       @history_size = history_size ? history_size : HistorySize.new(10)
       @tick = tick ? tick : Tick.new(0)
 
-      @state_default = case
-                       when state_default && state_allowed?(state_default)
-                         state_default
-                       when @states_allowed.nil? || @states_allowed.empty?
-                         STATE_NOT_SET
-                       else
-                         @states_allowed.first
-                       end
+      @state_default = state_default ? state_default : State.values.first
       @state = state ? state : @state_default
 
-      @history = history ? history : StateChangeHistory.new
-      @paths_allowed = paths_allowed ? paths_allowed : PathsAllowed.new
+      # @history = history ? history : StateChangeHistory.new # TODO!
+      @paths_allowed = paths_allowed ? paths_allowed : initial_default_path
 
       @errors = validate
     end
 
-    # ameba:enable Metrics/CyclomaticComplexity
-
     def validate
       errors = CurrentErrors.new
 
-      errors[:states_allowed] = "must be populated with states" if @states_allowed.empty?
-      errors[:state_default] = "must be one of state_allowed" unless state_allowed?(state_default)
-      errors[:states] = "must be set" if @state == :error_state_unset
-      errors[:states] = "must be one of state_allowed" unless state_allowed?(@state)
-      errors[:paths_allowed] = "must be an mapping of state to array of states" if @paths_allowed.keys.empty?
+      errors[ERROR_KEY_PATHS_ALLOWED] = "must be an mapping of state to array of states" if @paths_allowed.keys.empty?
 
       errors
     end
@@ -88,17 +95,28 @@ module Smcr
 
     ####
 
-    def state_allowed?(state_example)
-      @states_allowed.includes?(state_example)
-    end
+    # def state_allowed?(state_example)
+    #   @states_allowed.includes?(state_example)
+    # end
 
     def paths_allowed?(state_from, state_to)
       @paths_allowed.keys.includes?(state_from) && @paths_allowed[state_from].includes?(state_to)
     end
 
+    def initial_default_path
+      value_first = State.values.first.value
+      values_all = State.values.map(&.value) # .map{|val| val}
+      values_other = values_all - [value_first]
+      {value_first => values_other}
+    end
+
     def add_path(state_from, state_to)
-      @paths_allowed[state_from] = StatesAllowed.new unless @paths_allowed.keys.includes?(state_from)
-      @paths_allowed[state_from] << state_to
+      @paths_allowed[state_from.value] = StatesAllowed.new unless @paths_allowed.keys.includes?(state_from.value)
+
+      # You can effectively re-order 'state_to' values by re-add a 'state_to' value (which gets moved to the end).
+      @paths_allowed[state_from.value].delete(state_to.value) if @paths_allowed[state_from.value].includes?(state_to.value)
+
+      @paths_allowed[state_from.value] << state_to.value
     end
   end
 end
